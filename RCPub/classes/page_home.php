6876572ffa7160b02158d1dc30926dd8ad1bf14f
Blaine Myers <beemfx@gmail.com>
2012-05-05 10:41:50 -0700

    Twitter now dependent on settings.

--- RCPub/classes/page_home.php
+++ RCPub/classes/page_home.php
@@ -62,9 +62,7 @@ class CPageHome extends CPageBase {
 	{
 		$NewsTable = new CTableNews($this->m_db);
 		$nNumStories = $this->GetGlobalSetting('nHomeNewsStories');
-		echo 'There should be '.$nNumStories.' stories.';
 		$nNumStories = $NewsTable->ObtainRecentNews($nNumStories);
-		echo 'There are '.$nNumStories.' stories.';
 		print('<div class="news">');
 		printf('<h2><a class="tlink" href=%s>News</a></h2>', CreateHREF(PAGE_NEWS, 'archive'));
 
@@ -88,12 +86,11 @@ class CPageHome extends CPageBase {
 		print '<a class="big_link" href='.CreateHREF(PAGE_NEWS, 'archive').'>more news</a>';
 	}
 
-	private function ShowTwitter() {
-		?>
-		<h2><a class="tlink" href="http://twitter.com/RoughConcept">On Twitter</a></h2>
-		<ul id="twitter_update_list"></ul>
-		<a class="big_link" href="http://twitter.com/RoughConcept" id="twitter-link" style="text-align:right;">follow me on Twitter</a>
-		<?php
+	private function ShowTwitter() 
+	{			
+		echo '<h2><a class="tlink" href="http://twitter.com/RoughConcept">On Twitter</a></h2>';
+		echo '<ul id="twitter_update_list"></ul>';
+		echo '<a class="big_link" href="http://twitter.com/'.$this->GetGlobalSetting('txtTwitterUser').'" id="twitter-link" style="text-align:right;">follow me on Twitter</a>';
 	}
 
 	private function ShowBlog() {
@@ -102,31 +99,17 @@ class CPageHome extends CPageBase {
 		print '<a class="big_link" href="http://www.roughconcept.com/blog/">more blog posts</a>';
 	}
 
-	private function ShowFeature() {
-	//Will bring back the featured content once some real content has been uploaded.
-	//Only want the first volume of a series, or any content that doesn't have a volume.
-		$res = $this->DoQuery('select id, '.FULL_TITLE_QUERY.' as txtFullTitle, txtDesc, txtFile from tblContent where (fVolume is null or fVolume = 1) and bVisible=1');
-		if($res==true) {
-			$day = getdate();
-			srand($day['yday']);
-			$res->data_seek(rand(0, $res->num_rows));
-			$row = $res->fetch_assoc();
-			print("<h2><a class=\"tlink\" href=".CreateHREF(PAGE_TOC).">Daily Feature</a></h2>\n");
-			print("<h4><a target=\"_blank\" style=\"color:black;font-weight:bold\" href="./*CreateHREF(PAGE_CONTENT, "id=".$row['id'])*/$row['txtFile'].">".$row['txtFullTitle']."</a></h4>\n");
-			print("<p>".neat_trim($row['txtDesc'], 550)."</p>\n");
-
-			$res->free();
-		}
-
-		print '<a class="big_link" href='.CreateHREF(PAGE_CONTENT, 'id='.$row['id']).'>notes and comments</a>';
-		print '<a class="big_link" target=\"_blank\" href='./*CreateHREF(PAGE_CONTENT, 'id='.$row['id'])*/$row['txtFile'].'>read it</a>';
+	private function ShowFeature()
+	{
+		?>
+		A feature goes here.
+		<?php
 	}
 
-	protected function DisplayPost() {
-		?>
-<script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>
-<script type="text/javascript" src="http://twitter.com/statuses/user_timeline/RoughConcept.json?callback=twitterCallback2&amp;count=5"></script>
-	<?php
+	protected function DisplayPost() 
+	{
+	echo '<script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>';
+	echo '<script type="text/javascript" src="http://twitter.com/statuses/user_timeline/'.$this->GetGlobalSetting('txtTwitterUser').'.json?callback=twitterCallback2&amp;count=5"></script>';
 	}
 }
 ?>
