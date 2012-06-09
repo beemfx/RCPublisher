9a517ab92d0aa89dd496421cfdef105d22d1a788
Blaine Myers <beemfx@gmail.com>
2012-06-09 13:14:54 -0700

    Can now create new users. Only an existing user can create a new user.

--- RCPub/classes/page_user.php
+++ RCPub/classes/page_user.php
@@ -28,6 +28,10 @@ class CPageUser extends CPageBase
 		{
 			$this->UpdatePassword();
 		}
+		else if($_POST['stage'] == 'nuser')
+		{
+			$this->InsertNewUser();
+		}
 	}
 
 	protected function DisplayContent()
@@ -36,11 +40,14 @@ class CPageUser extends CPageBase
 		RCError_ShowErrors();
 		//No matter what we display the form.
 		$this->DisplayChangePassword();
+		
+		$this->DisplayCreateNewUser();
 	}
 	
 	private function DisplayChangePassword()
 	{
 		?>
+		<h3>Change Password</h3>
 		<div style="width:100%;margin:0;padding:1em">
 		<form action=<?php print CreateHREF(PAGE_USER)?> method="post" name="PChangeForm">
 			
@@ -51,6 +58,28 @@ class CPageUser extends CPageBase
 		<tr><th>Confirm New:</th><td><input type="password" name="npassc" value="<?php ?>" style="width:50%"/></td></tr>
 		</table>
 		<center><input class="button" type="button" value="Submit" onclick="javascript:onSubmitPChange()"/></center>
+		</form>
+		</div>
+		<?php
+	}
+	
+	private function DisplayCreateNewUser()
+	{
+		?>
+		<h3>Create New User</h3>
+		<div style="width:100%;margin:0;padding:1em">
+		<form action=<?php print CreateHREF(PAGE_USER)?> method="post" name="NewUserForm">
+			
+		<input type="hidden" name="stage" value="nuser"/>
+		<table style ="width:50%">
+		<tr><th>Username:</th><td><input type="text" name="uname" value="<?php echo $_POST['uname']?>" style="width:50%"/></td></tr>
+		<tr><th>Alias:</th><td><input type="text" name="ualias" value="<?php echo $_POST['ualias']?>" style="width:50%"/></td></tr>
+		<tr><th>Email:</th><td><input type="text" name="uemail" value="<?php echo $_POST['uemail'] ?>" style="width:50%"/></td></tr>
+		<tr><th>Access Level:</th><td><input type="text" name="uaccess" value="<?php echo $_POST['uaccess']?>" style="width:50%"/></td></tr>
+		<tr><th>Password:</th><td><input type="password" name="npass" value="<?php ?>" style="width:50%"/></td></tr>
+		<tr><th>Confirm Password:</th><td><input type="password" name="npassc" value="<?php ?>" style="width:50%"/></td></tr>
+		</table>
+		<center><input class="button" type="submit" value="Submit"/></center>
 		</form>
 		</div>
 		<?php
@@ -74,6 +103,17 @@ class CPageUser extends CPageBase
 		
 		//We're good to go, stuff it in.
 		$this->m_UserTable->SetUserPassword((int)RCSession_GetUserProp('user_id'), $_POST['npass']);
+		RCError_PostError('Password updated.', 2);
+	}
+	
+	protected function InsertNewUser()
+	{
+		if($_POST['npass'] != $_POST['npassc'])
+		{
+			RCError_PostError('Passwords must match');
+			return;
+		}
+		$this->m_UserTable->InsertNew($_POST['uname'], $_POST['ualias'], $_POST['uemail'], (int)$_POST['uaccess'], $_POST['npass']);
 	}
 	
 	protected function DisplayPost()
--- RCPub/classes/table_user.php
+++ RCPub/classes/table_user.php
@@ -20,6 +20,57 @@ class CTableUser extends CTable
 		return count($this->m_rows) == 1 ? (int)$this->m_rows[0]['id'] : false;		
 	}
 	
+	public function InsertNew($sUname, $sAlias, $sEmail, $nAccess, $sPass)
+	{
+		assert('integer' == gettype($nAccess));
+		
+		//We'll do some verification to make sure this is valid:
+		if(!preg_match('/^[A-Za-z0-9]{6,}$/', $sUname))
+		{
+			RCError_PostError('User names may only contain letters and numbers and must be at least 6 charactrs long.');
+			return false;
+		}
+		
+		if(!preg_match('/^[A-Za-z0-9_ ]*$/', $sAlias))
+		{
+			RCError_PostError('Aliases may only contain leters numbers and -,_');
+			return false;
+		}
+		
+		$strEmailRegEx = "/^[^0-9][A-z0-9_]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/";
+
+		if(!preg_match($strEmailRegEx, $sEmail))
+		{
+			RCError_PostError('That email address isn\'t valid.');
+			return false;
+		}
+		
+		if( !(1<=$nAccess && $nAccess <=10))
+		{
+			RCError_PostError('The access level must be between 1 and 10');
+			return false;
+		}
+		
+		if(!preg_match('/^[^ ]{6,}$/', $sPass))
+		{
+			RCError_PostError('Passwords must be 6 characters long and cannot contain spaces.');
+			return false;
+		}
+		
+		//We are now ready to insert.
+		$data = array
+		(
+			 'txtUserName' => '"'.addslashes($sUname).'"',
+			 'txtPassword' => 'md5("'.addslashes($sPass).'")',
+			 'txtAlias'    => '"'.addslashes($sAlias).'"',
+			 'txtEmail'    => '"'.addslashes($sEmail).'"',
+			 'nAccessLevel'=> $nAccess,
+		);
+		
+		$this->DoInsert($data);
+		return true;
+	}
+	
 	public function GetUserInfo($nID)
 	{
 		$this->DoSelect('id, txtUserName, txtAlias, txtEmail, nAccessLevel, txtLastIP', 'id='.$nID);
@@ -40,6 +91,12 @@ class CTableUser extends CTable
 	
 	public function SetUserPassword($nID, $sPass)
 	{
+		if(!preg_match('/^[^ ]{6,}$/', $sPass))
+		{
+			RCError_PostError('Passwords must be 6 characters long and cannot contain spaces.');
+			return false;
+		}
+		
 		$data = array
 		(
 			 'txtPassword' => '"'.$sPass.'"',
